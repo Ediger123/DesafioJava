04a582260f8ae4803b47f00ca1218e22ef181218
Ediger123 <gomesluizeduardo557@gmail.com>
2023-05-19 09:03:03 -0300

    Desafio Java

---
+++ codigos.js
@@ -0,0 +1,22 @@
+
+
+function ativarNome(){
+    let nome = 'Eduardo';
+
+    console.log(nome)
+    document.getElementById('aparecer').innerHTML = nome
+    //document.write(nome)
+}
+
+
+function somarNumeros(){
+    let a = 5;
+    let b = 10;
+
+
+    let c = a*b;
+
+    //console.log(c)
+    //document.write(c)
+    document.getElementById('aparecer').innerHTML = c
+}
